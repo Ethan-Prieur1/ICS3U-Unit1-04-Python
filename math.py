46d0767a64145d4207792f53d0dcdf728f41171c
Ethan Prieur <ethan.prieur@mths.ca>
2022-02-24 19:42:43 +0000

    Added Math File

---
+++ math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Ethan Prieur
+# Created on February 24th 2022
+# This is a Program to do Easy Math
+
+
+
+def main():
+    #This Does Basic Math
+    
+    print("A Rectangle has the Dimensions of 5cm x 3cm")
+    print("")
+    print ("Area will be {}cm².".format(3 * 5))
+    print("Perimeter will be {}cm.".format(2 * ( 3 + 5)))
+    
+
+
+if __name__ == "__main__":
+    main()
