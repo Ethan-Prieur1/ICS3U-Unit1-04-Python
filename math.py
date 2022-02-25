25191a177d44b2264083d5d4efd7192aad09cfb6
Ethan Prieur <ethan.prieur@mths.ca>
2022-02-25 19:24:31 +0000

    Added Math File Thing

--- math.py
+++ math.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
-# Created by Ethan Prieur
-# Created on February 24th 2022
-# This is a Program to do Easy Math
-
+# Created by: Ethan Prieur
+# Created on: February 24th
+# This is a programt that does simple math
 
 
 def main():
-    #This Does Basic Math
-    
-    print("A Rectangle has the Dimensions of 5cm x 3cm")
-    print("")
-    print ("Area will be {}cm².".format(3 * 5))
-    print("Perimeter will be {}cm.".format(2 * ( 3 + 5)))
-    
+    # this function does the simple math
+
+    print(9 + 2)
+    print("7-3={}".format(7 - 3))
+    print("4/2={}".format(4 / 2))
+    print("4+4/2={}".format(4 + 4 / 2))
+    print("3+4*2={}".format(3 + 4 * 2))
+    print("5+2^3={}".format(5 + 2 ^ 3))
+    print("5+2³={}".format(5 + 2**3))
 
 
 if __name__ == "__main__":
